c69acdc121cd8e3ae1e9eaa8d7face6383d5780f
phamtienanh157 <phamtienanh157@gmail.com>
2021-05-10 11:16:53 +0700

    release 2.0

---
+++ main.js
@@ -0,0 +1,15 @@
+const x = document.getElementById("box").offsetWidth;
+const y = document.getElementById("box").offsetHeight;
+const height = window.innerHeight - y;
+const width = window.innerWidth - x;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function changeBtn() {
+  let valueHeight = getRandomInt(0, height);
+  let valueWidth = getRandomInt(0, width);
+  document.getElementById("box").style.marginTop = valueHeight + "px";
+  document.getElementById("box").style.marginLeft = valueWidth + "px";
+}
